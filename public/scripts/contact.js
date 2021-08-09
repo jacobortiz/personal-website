8bed5aedf90d7e04ae1e36376f80b111c9fe4a7d
Jacob Ortiz <jacobortiz90@gmail.com>
2021-08-08 19:59:18 -0700

    projects section added

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -3,29 +3,34 @@ $(function() {
     $('#contact-form').on('submit', function(e) {
         if(!e.isDefaultPrevented()) {
 
-            $.ajax({
-                type: 'POST',
-                url: '/contact',
-                data: $(this).serialize(),
-                success: function(data) {
+            console.log('defualt prevented')
 
-                    // recieve type of message
-                    var message_alert = 'alert-' + data.type;
-                    var message_text = data.message;
+            // $.ajax({
+            //     type: 'POST',
+            //     url: '/contact',
+            //     data: $(this).serialize(),
+            //     success: function(data) {
 
-                    var alert_box = `
-                        <div class='alert ${message_alert} alert-dismissable'>
-                            <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;></button>${message_text}</div>
-                    `;
+            //         console.log('success')
 
-                    if(message_alert && message_text) {
-                        // inject alert into .message
-                        $('#contact-form').find('.messages').html(alert_box)
-                        $('.#contact-form')[0].reset()
-                    }
-                }
-            });
-            return false;
+            //         // recieve type of message
+            //         var message_alert = 'alert-' + data.type;
+            //         var message_text = data.message;
+
+            //         var alert_box = `
+            //             <div class='alert ${message_alert} alert-dismissable'>
+            //                 <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;></button>${message_text}</div>
+            //         `;
+
+            //         if(message_alert && message_text) {
+            //             // inject alert into .message
+            //             $('#contact-form').find('.messages').html(alert_box)
+            //             $('.#contact-form')[0].reset()
+            //         }
+            //     }
+            // });
+
+            // return false;
         }
     });
 });
--- routes/contact.js
+++ routes/contact.js
@@ -8,8 +8,5 @@ router.get('/', function(req, res, next) {
     });
 });
 
-router.post('/', function(req, res, next) {
-
-})
 
 module.exports = router
