0fe5221c667d40c51a08e0bfec66ee9ffb4940a7
jacobortiz <jacobortiz90@gmail.com>
2021-08-10 19:20:09 -0700

    contact page working

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -1,33 +1,3 @@
 $(function() {
 
-    $('#contact-form').on('submit', function(e) {
-        if(!e.isDefaultPrevented()) {
-            $.ajax({
-                type: 'POST',
-                url: '/contact',
-                data: $(this).serialize(),
-                success: function(data) {
-
-                    console.log('success')
-
-                    // recieve type of message
-                    var message_alert = 'alert-' + data.type;
-                    var message_text = data.message;
-
-                    var alert_box = `
-                        <div class='alert ${message_alert} alert-dismissable'>
-                            <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;></button>${message_text}</div>
-                    `;
-
-                    if(message_alert && message_text) {
-                        // inject alert into .message
-                        $('#contact-form').find('.messages').html(alert_box)
-                        $('.#contact-form')[0].reset()
-                    }
-                }
-            });
-
-            // return false;
-        }
-    });
 });
--- routes/contact.js
+++ routes/contact.js
@@ -1,16 +1,66 @@
 var express = require('express');
 var router = express.Router();
 
+var nodemailer = require('nodemailer')
+
+const name = 'Jacob Ortiz-Rodriguez'
+
 /* GET contact page. */
 router.get('/', function(req, res, next) {
     res.render('contact', { 
-        name: 'Jacob Ortiz-Rodriguez',
+        name: name,
+        status: 'muted', 
+        message: '* Required'
     });
 });
 
 /* POST contact */
 router.post('/', function(req, res, next) {
-    res.send(process.env.EMAIL)
+    var name = req.body.name
+    var surname = req.body.surname
+    var email = req.body.email
+    var message = req.body.message
+
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail', 
+        auth: {
+            type: 'OAuth2',
+            user: process.env.EMAIL,
+            clientId: process.env.clientId,
+            clientSecret: process.env.clientSecret,
+            refreshToken: process.env.refreshToken,
+            accessToken: process.env.accessToken
+        }
+    });
+
+    var mail_options = {
+        from: email,
+        to: 'jacob@jacobortiz.io',
+        subject: `${name} ${surname} | New Message!`,
+        text: message
+    }
+
+    transporter.sendMail(mail_options, function(error, response) {
+        if(error) {
+            console.log(error)
+            res.render('contact', {
+                name: name,
+                status: 'danger',
+                message: 'Oops, something went wrong, please try again in a bit.'
+
+            })
+
+        } else {
+            res.render('contact', {
+                name: name,
+                status: 'success',
+                message: 'Message sent successfully, I will get back to you soon!'
+            })
+        }
+
+
+    });
 })
 
 module.exports = router
